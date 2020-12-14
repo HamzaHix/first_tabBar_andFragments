5840c8735e26b267e6da7b09fb0f59282cefe5d9
hamzahix <hred775@gmail.com>
2020-12-14 00:39:57 +0000

    first_tabBar_andFragments v1 continue the project

--- app/src/main/java/com/hixman/first_tabbar_andfragments/MainActivity.kt
+++ app/src/main/java/com/hixman/first_tabbar_andfragments/MainActivity.kt
@@ -3,19 +3,48 @@ package com.hixman.first_tabbar_andfragments
 import android.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.hixman.first_tabbar_andfragments.Fragments.CategoryFragment
+import com.hixman.first_tabbar_andfragments.Fragments.HomeFragment
+import com.hixman.first_tabbar_andfragments.Fragments.MyListFragment
+import com.hixman.first_tabbar_andfragments.Fragments.SettingsFragment
 import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 
 class MainActivity : AppCompatActivity() {
 
 
+    private lateinit var navBar:ChipNavigationBar
+    private  var fragment:Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadFragment()
+        loadFragment(HomeFragment())
+        navBar= findViewById(R.id.navBar)
+
+        navBar.setOnItemSelectedListener { id ->
+
+            fragment = null
+            when(id){
+
+                R.id.home ->{
+                    fragment = HomeFragment()
+                }
+                R.id.myList ->{
+                    fragment = MyListFragment()
+                }
+                R.id.category ->{
+                    fragment = CategoryFragment()
+                }
+                R.id.settings ->{
+                    fragment = SettingsFragment()
+                }
+            }
+            loadFragment(fragment!!)
+        }
 
     }
 
@@ -23,6 +52,7 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragment_container,fragment)
             .commit()
+
     }
 
 
